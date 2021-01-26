3b16610d7b6a4a4a2dd41406af3f9abdaba8d68e
Jon Hill <jondhill1984@gmail.com>
2021-01-26 16:52:39 +0200

    Implement basic new task creation from new page

--- pages/task/new.tsx
+++ pages/task/new.tsx
@@ -1,10 +1,78 @@
 import Head from "next/head";
-import React from "react";
+import React, { useState } from "react";
 // import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
-// import styles from "./index.module.scss";
+import styles from "./new.module.scss";
 
-// export const TaskContext = React.createContext(
+export default function NewTask() {
+  const { container, taskForm } = styles;
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+    estimatedDueDate: "",
+  });
 
-export default function Home() {
-  return <div> New Task page</div>;
+  function handleChange(e) {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    createTask();
+  }
+
+  async function createTask() {
+    try {
+      const res = await fetch("http://localhost:3000/api/tasks", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <main className={container}>
+        <div> New Task page</div>
+        <form onSubmit={handleSubmit} className={taskForm}>
+          <label htmlFor="title">Task title</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Title of the task"
+            onChange={handleChange}
+            required
+          />
+          <label htmlFor="description">Task description</label>
+          <textarea
+            id="description"
+            name="description"
+            rows="5"
+            cols="40"
+            placeholder="Write a comment..."
+            onChange={handleChange}
+            required
+          ></textarea>
+          <label htmlFor="estimatedDueDate">Estimated due date</label>
+          <input
+            type="date"
+            id="estimatedDueDate"
+            name="estimatedDueDate"
+            placeholder="Enter a date to help build your visusal timeline"
+            onChange={handleChange}
+          />
+          <input type="submit" value="Submit"></input>
+        </form>
+      </main>
+    </>
+  );
 }
