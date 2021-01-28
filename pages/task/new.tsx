0b53e8c62f4364f609bea42fadbb7416934ed0ce
Jon Hill <jondhill1984@gmail.com>
2021-01-28 16:55:33 +0200

    Move New Task implementation from page to own component

--- pages/task/new.tsx
+++ pages/task/new.tsx
@@ -1,80 +1,16 @@
 import Head from "next/head";
-import React, { useState } from "react";
+import React from "react";
 import styles from "./new.module.scss";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
+import TaskModal from "../../components/taskModal/TaskModal";
 
-export default function NewTask() {
-  const { container, taskForm } = styles;
-  const [form, setForm] = useState({
-    title: "",
-    description: "",
-    estimatedDueDate: "",
-  });
-  const router = useRouter();
-
-  function handleChange(e) {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    createTask();
-  }
-
-  async function createTask() {
-    try {
-      const res = await fetch("http://localhost:3000/api/tasks", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(form),
-      });
-      router.push("/home");
-    } catch (error) {
-      console.log(error);
-    }
-  }
+export default function New() {
+  const {} = styles;
 
   return (
     <>
-      <main className={container}>
-        <div> New Task page</div>
-        <form onSubmit={handleSubmit} className={taskForm}>
-          <label htmlFor="title">Task title</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            placeholder="Title of the task"
-            onChange={handleChange}
-            required
-          />
-          <label htmlFor="description">Task description</label>
-          <textarea
-            id="description"
-            name="description"
-            rows="5"
-            cols="40"
-            placeholder="Write a comment..."
-            onChange={handleChange}
-            required
-          ></textarea>
-          <label htmlFor="estimatedDueDate">Estimated due date</label>
-          <input
-            type="date"
-            id="estimatedDueDate"
-            name="estimatedDueDate"
-            placeholder="Enter a date to help build your visusal timeline"
-            onChange={handleChange}
-          />
-          <input type="submit" value="Submit"></input>
-        </form>
-      </main>
+      <div> New Task page</div>
+      <TaskModal />
     </>
   );
 }
