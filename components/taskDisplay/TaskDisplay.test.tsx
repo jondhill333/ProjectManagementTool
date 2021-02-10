bb9b5088de179bbe98c3d0cef0a489dc72f7c084
Jon Hill <jondhill1984@gmail.com>
2021-02-10 15:52:19 +0200

    Set up task display component with first basic tests

--- components/taskDisplay/TaskDisplay.test.tsx
+++ components/taskDisplay/TaskDisplay.test.tsx
@@ -2,37 +2,82 @@ import TaskDisplay from "./TaskDisplay.tsx";
 import React from "react";
 import Enzyme, { shallow, mount, render } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
+import { findByTestAttribute, checkProps } from "../../test/testUtils";
+// import { ITask } from "../../models/Task";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
-
-const setup = () => shallow(<TaskDisplay />);
-
-export const findByTestAttribute = (wrapper, val) => {
-  return wrapper.find(`[data-test='${val}']`);
+const defaultProps = {
+  task: { title: 22 },
+  // title: 22,
+  // description: "can you hear me",
+  // taskNumber: 1,
+  // test: "wagwan",
 };
+
+const setup = (props) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<TaskDisplay {...setupProps} />);
+};
+
+test("does not throw warning with expected props", () => {
+  checkProps(TaskDisplay, defaultProps);
+  // console.log(test.debug());
+});
 
 describe("component appearance is correct", () => {
   test("should render without throwing an eror", () => {
-    const wrapper = setup();
+    const wrapper = setup({});
     const container = findByTestAttribute(wrapper, "container");
     expect(container.length).toBe(1);
   });
 
-  test("should render the div for the project title", () => {
-    const wrapper = setup();
+  test("should render the node for the project title", () => {
+    const wrapper = setup({});
     const projectTitle = findByTestAttribute(wrapper, "projectTitle");
     expect(projectTitle.length).toBe(1);
   });
 
-  test("should render the span for the project task number", () => {
-    const wrapper = setup();
+  test("should render the node for the project task number", () => {
+    const wrapper = setup({});
     const projectTaskNumber = findByTestAttribute(wrapper, "projectTaskNumber");
     expect(projectTaskNumber.length).toBe(1);
   });
 
-  test("should render the div for the task title", () => {
-    const wrapper = setup();
+  test("should render the node for the task title", () => {
+    const wrapper = setup({});
     const taskTitle = findByTestAttribute(wrapper, "taskTitle");
     expect(taskTitle.length).toBe(1);
   });
 });
+
+// describe("render the props correctly", () => {
+//   let wrapper;
+
+//   const testProject = {
+//     title: "project one",
+//     description: "project one",
+//   };
+
+//   beforeEach(() => {
+//     wrapper = setup({ testProject });
+//   });
+
+//   test("should render without throwing an error", () => {
+//     const container = findByTestAttribute(wrapper, "container");
+//     expect(container.length).toBe(1);
+//   });
+//   test("project title node is not empty", () => {
+//     const projectTitle = findByTestAttribute(wrapper, "projectTitle");
+//     expect(projectTitle.text().length).not.toBe(0);
+//   });
+
+//   test("project Task Number node is not empty", () => {
+//     const projectTaskNumber = findByTestAttribute(wrapper, "projectTaskNumber");
+//     expect(projectTaskNumber.text().length).not.toBe(0);
+//   });
+
+//   test("task title node is not empty", () => {
+//     const taskTitle = findByTestAttribute(wrapper, "taskTitle");
+//     expect(taskTitle.text().length).not.toBe(0);
+//   });
+// });
--- components/taskDisplay/TaskDisplay.tsx
+++ components/taskDisplay/TaskDisplay.tsx
@@ -1,15 +1,22 @@
 import React, { useState, useEffect, useContext } from "react";
+import { ITask } from "../../models/Task";
 import styles from "./TaskDisplay.module.scss";
-// import { useRouter } from "next/router";
-// import ProjectContext from "../../util/ProjectContext";
-// import { ProjectCountContext } from "../../util/ProjectCountContext";
 
-export default function TaskDisplay() {
+export interface DisplayProps {
+  task: ITask;
+  currentProject: { id: string; title: string };
+}
+
+export default function TaskDisplay({ task, currentProject }: DisplayProps) {
   const { container, projectTitle, projectTaskNumber, taskTitle } = styles;
+  let project = <span>{currentProject}</span>;
   return (
     <>
       <div data-test="container" className={container}>
-        <div data-test="projectTitle" className={projectTitle}></div>
+        <div data-test="projectTitle" className={projectTitle}>
+          {/* {props.testProject.title} */}
+          {currentProject}
+        </div>
         <span
           data-test="projectTaskNumber"
           className={projectTaskNumber}
